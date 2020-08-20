e50c5c23688ed0270f3bb6c2cd01eba1c53b1421
Carol <947752894@qq.com>
2020-08-20 16:48:39 +0800

    Update MongoClientsInit.java

--- src/main/java/site/cnkj/utils/MongoClientsInit.java
+++ src/main/java/site/cnkj/utils/MongoClientsInit.java
@@ -4,7 +4,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import lombok.Data;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -14,17 +15,17 @@ import java.util.HashMap;
 
 /*
  * @author  Carol
- * @create  2020/8/14 15:50
+ * @create  2020/8/17 9:50
  * @Description
  */
 @Component
 @Data
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
 @Configuration
 @ConfigurationProperties(prefix = "spring.data.mongodb")
-@ConditionalOnProperty(prefix = "spring.data.mongodb", name = "activity", havingValue = "true")
 public class MongoClientsInit {
 
-    private HashMap<String, String> uri;
+    private HashMap<String, String> uri = new HashMap<>();
 
     private HashMap<String, MongoDatabase> mongoClientDatabases = new HashMap<>();
 
@@ -44,13 +45,16 @@ public class MongoClientsInit {
 
     @PostConstruct
     public void init() throws Exception{
-        for (String name : uri.keySet()) {
-            String url = uri.get(name);
-            MongoClient mongoClient = MongoClients.create(url);
-            MongoDatabase mongoClientDatabase = mongoClient.getDatabase(getDatabase(url));
-            mongoClientDatabases.put(name, mongoClientDatabase);
+        if (uri.size() > 0){
+            for (String name : uri.keySet()) {
+                String url = uri.get(name);
+                MongoDatabase mongoClientDatabase;
+                try (MongoClient mongoClient = MongoClients.create(url)) {
+                    mongoClientDatabase = mongoClient.getDatabase(getDatabase(url));
+                }
+                mongoClientDatabases.put(name, mongoClientDatabase);
+            }
         }
     }
-
 
 }
