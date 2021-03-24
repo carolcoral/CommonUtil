8cabbe6b122075dddb8092716900c08dd7b9e4c6
liuxuewen <liuxuewen_fh@12530.com>
2021-03-24 15:21:35 +0800

    change MongoClientInit and fix some bug.

--- src/main/java/site/cnkj/common/config/MongoClientConfig.java
+++ src/main/java/site/cnkj/common/config/MongoClientConfig.java
@@ -1,20 +1,14 @@
 package site.cnkj.common.config;
 
 
-import com.mongodb.*;
-import com.mongodb.selector.ServerSelector;
+import com.mongodb.MongoClientOptions;
 import lombok.Setter;
-import org.bson.UuidRepresentation;
-import org.bson.codecs.configuration.CodecRegistry;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import site.cnkj.common.utils.data.MongoClientsInit;
 
-import javax.net.ssl.SSLContext;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 
 /*
  * @author  LXW
@@ -31,43 +25,31 @@ public class MongoClientConfig {
 
 
     private HashMap<String, String> uri = new HashMap<>();
-
     private String applicationName;
-    private List<MongoCompressor> compressorList = Collections.emptyList();
-    private ReadPreference readPreference = ReadPreference.primary();
-    private WriteConcern writeConcern = WriteConcern.ACKNOWLEDGED;
-    private boolean retryWrites = true;
-    private boolean retryReads = true;
-    private ReadConcern readConcern = ReadConcern.DEFAULT;
-    private CodecRegistry codecRegistry = MongoClient.getDefaultCodecRegistry();
-    private UuidRepresentation uuidRepresentation = UuidRepresentation.UNSPECIFIED;
-    private ServerSelector serverSelector;
-    private int minConnectionsPerHost;
-    private int maxConnectionsPerHost = 100;
-    private int serverSelectionTimeout = 1000 * 30;
-    private int maxWaitTime = 1000 * 60 * 2;
-    private int maxConnectionIdleTime;
-    private int maxConnectionLifeTime;
-    private int connectTimeout = 1000 * 10;
-    private int socketTimeout = 0;
-    private boolean sslEnabled = false;
-    private boolean sslInvalidHostNameAllowed = false;
-    private SSLContext sslContext;
+    private Boolean retryWrites = true;
+    private Boolean retryReads = true;
+    private Integer minConnectionsPerHost;
+    private Integer maxConnectionsPerHost = 100;
+    private Integer serverSelectionTimeout = 1000 * 30;
+    private Integer maxWaitTime = 1000 * 60 * 2;
+    private Integer maxConnectionIdleTime;
+    private Integer maxConnectionLifeTime;
+    private Integer connectTimeout = 1000 * 10;
+    private Integer socketTimeout = 0;
+    private Boolean sslEnabled = false;
+    private Boolean sslInvalidHostNameAllowed = false;
 
-    private int heartbeatFrequency = 10000;
-    private int minHeartbeatFrequency = 500;
-    private int heartbeatConnectTimeout = 20000;
-    private int heartbeatSocketTimeout = 20000;
-    private int localThreshold = 15;
+    private Integer heartbeatFrequency = 10000;
+    private Integer minHeartbeatFrequency = 500;
+    private Integer heartbeatConnectTimeout = 20000;
+    private Integer heartbeatSocketTimeout = 20000;
+    private Integer localThreshold = 15;
 
 
     private MongoClientOptions.Builder builder(){
         //配制连接池
         MongoClientOptions.Builder mongoClientBuilder = new MongoClientOptions.Builder();
         mongoClientBuilder.applicationName(applicationName);
-        mongoClientBuilder.autoEncryptionSettings(AutoEncryptionSettings.builder().build());
-        mongoClientBuilder.codecRegistry(codecRegistry);
-        mongoClientBuilder.compressorList(compressorList);
         mongoClientBuilder.connectionsPerHost(maxConnectionsPerHost);
         mongoClientBuilder.connectTimeout(connectTimeout);
         mongoClientBuilder.heartbeatConnectTimeout(heartbeatConnectTimeout);
@@ -79,18 +61,12 @@ public class MongoClientConfig {
         mongoClientBuilder.maxWaitTime(maxWaitTime);
         mongoClientBuilder.minConnectionsPerHost(minConnectionsPerHost);
         mongoClientBuilder.minHeartbeatFrequency(minHeartbeatFrequency);
-        mongoClientBuilder.readConcern(readConcern);
-        mongoClientBuilder.readPreference(readPreference);
         mongoClientBuilder.retryReads(retryReads);
         mongoClientBuilder.retryWrites(retryWrites);
         mongoClientBuilder.serverSelectionTimeout(serverSelectionTimeout);
-        mongoClientBuilder.serverSelector(serverSelector);
         mongoClientBuilder.socketTimeout(socketTimeout);
-        mongoClientBuilder.sslContext(sslContext);
         mongoClientBuilder.sslEnabled(sslEnabled);
         mongoClientBuilder.sslInvalidHostNameAllowed(sslInvalidHostNameAllowed);
-        mongoClientBuilder.uuidRepresentation(uuidRepresentation);
-        mongoClientBuilder.writeConcern(writeConcern);
         return mongoClientBuilder;
     }
 
